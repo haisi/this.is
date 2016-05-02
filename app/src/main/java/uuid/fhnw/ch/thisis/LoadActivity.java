20ad4b366dc29ce89587615a6739a51f87de9eb4
Hasan Kara <hasan.kara@fhnw.ch>
2016-05-02 16:23:36 +0200

    Create empty load activity

---
+++ app/src/main/java/uuid/fhnw/ch/thisis/LoadActivity.java
@@ -0,0 +1,55 @@
+package uuid.fhnw.ch.thisis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Date;
+
+/**
+ * @author Hasan Kara <hasan.kara@fhnw.ch>
+ */
+public class LoadActivity extends Activity {
+
+    private static final long MIN_MILLISECONDS_OF_SPLASH_SCREEN = 3500;
+    private Date startTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+
+        startTime = new Date();
+
+        openNextActivityWhenMinTimeHasPassed(MainActivity.class);
+    }
+
+    /**
+     * Checks if the splash screen has been displayed the min amount of milliseconds
+     * and starts the passed activity.
+     *
+     * @param clazz activity to start
+     */
+    private void openNextActivityWhenMinTimeHasPassed(Class clazz) {
+        final Intent intent = new Intent(getBaseContext(), clazz);
+
+        long timeSpend = new Date().getTime() - startTime.getTime();
+
+        if(timeSpend < MIN_MILLISECONDS_OF_SPLASH_SCREEN) {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    LoadActivity.this.finish();
+                }
+            }, MIN_MILLISECONDS_OF_SPLASH_SCREEN - timeSpend);
+
+        } else {
+            startActivity(intent);
+            this.finish();
+        }
+    }
+
+}
