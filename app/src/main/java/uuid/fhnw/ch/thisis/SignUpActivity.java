8df407231d59cf8061c3664f8a0982c91bd03ea5
Hasan Kara <hasan.kara@fhnw.ch>
2016-05-02 18:14:19 +0200

    Add simple sign-up page

---
+++ app/src/main/java/uuid/fhnw/ch/thisis/SignUpActivity.java
@@ -0,0 +1,17 @@
+package uuid.fhnw.ch.thisis;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Hasan Kara <hasan.kara@fhnw.ch>
+ */
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_screen);
+    }
+}
