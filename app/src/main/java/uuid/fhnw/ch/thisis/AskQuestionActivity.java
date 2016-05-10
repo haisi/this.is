1fbc96e315dffd44d397aa3f6a9560b3646acdce
Hasan Kara <hasan.kara@fhnw.ch>
2016-05-10 12:52:58 +0200

    Add toolbar to go back to the "ask question activity"

--- app/src/main/java/uuid/fhnw/ch/thisis/AskQuestionActivity.java
+++ app/src/main/java/uuid/fhnw/ch/thisis/AskQuestionActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * @author Hasan Kara <hasan.kara@fhnw.ch>
@@ -15,5 +17,22 @@ public class AskQuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask_question_screen);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
