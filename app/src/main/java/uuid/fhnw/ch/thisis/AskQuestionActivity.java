343dba41ba910bfa643e67bd2d874a04250784a6
Hasan Kara <hasan.kara@fhnw.ch>
2016-05-22 17:15:27 +0200

    Allow user to select an image for question

--- app/src/main/java/uuid/fhnw/ch/thisis/AskQuestionActivity.java
+++ app/src/main/java/uuid/fhnw/ch/thisis/AskQuestionActivity.java
@@ -1,6 +1,9 @@
 package uuid.fhnw.ch.thisis;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,17 +12,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Date;
+import java.util.Random;
 
 import uuid.fhnw.ch.thisis.business.DataService;
 import uuid.fhnw.ch.thisis.business.Question;
+import uuid.fhnw.ch.thisis.util.ImageUtils;
 
 /**
  * @author Hasan Kara <hasan.kara@fhnw.ch>
  */
 public class AskQuestionActivity extends AppCompatActivity {
+
+    private TextView optionalText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +43,10 @@ public class AskQuestionActivity extends AppCompatActivity {
 
         final EditText titleEdit = (EditText) findViewById(R.id.questionTitle);
         final EditText descriptionEdit = (EditText) findViewById(R.id.questionDescriptionEdit);
+
+        final ImageView newQuestionImage = (ImageView) findViewById(R.id.newQuestionImage);
+        optionalText = (TextView) findViewById(R.id.optionallyAddImage);
+        addListener(newQuestionImage);
 
         Button saveButton = (Button) findViewById(R.id.askQuestionButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +66,10 @@ public class AskQuestionActivity extends AppCompatActivity {
                     newQuestion.setDescription(description);
                 }
 
+                if (selectedImage != null) {
+                    newQuestion.setImageName(selectedImage);
+                }
+
                 Toast.makeText(getBaseContext(), "Saved your question!", Toast.LENGTH_LONG).show();
 
                 DataService.INSTACNE.currentUser().addQuestion(newQuestion);
@@ -62,6 +79,33 @@ public class AskQuestionActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    String selectedImage = null;
+
+    private final String[] images = {"cabel", "flowers", "plane"};
+
+    private void addListener(final ImageView newQuestionImage) {
+
+        final Context context = this;
+
+        final Random random = new Random();
+
+        newQuestionImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                optionalText.setVisibility(View.GONE);
+
+                Resources res = context.getResources();
+
+                String nextImage = images[random.nextInt(images.length)];
+                selectedImage = nextImage;
+
+                int resId = res.getIdentifier(nextImage, "drawable", context.getPackageName());
+                Bitmap bitmap = ImageUtils.decodeDownsampledBitmap(res, resId, newQuestionImage.getWidth(), newQuestionImage.getHeight());
+                newQuestionImage.setImageBitmap(bitmap);
+            }
+        });
     }
 
     @Override
